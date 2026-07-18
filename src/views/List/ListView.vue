92fab2d148681e1dd4a72449402b31333e15ccc1
int16 <int16int8@gmail.com>
2026-07-18 14:33:15 +0800

    feat: finetune ListView UI

--- src/views/List/ListView.vue
+++ src/views/List/ListView.vue
@@ -4,7 +4,7 @@ import { GetServerList, type ServerEntity } from '../../api/serverlist'
 import { GetLinkEntries, type LinkEntry } from '@/api/links'
 import useClipboard from 'vue-clipboard3'
 import ListItem from './ListItem.vue'
-import MinecraftButtonClassic from '@/components/utils/MinecraftButtonClassic.vue'
+import LinkItem from './LinkItem.vue'
 import { useToast } from 'vue-toastification'
 
 const toast = useToast()
@@ -117,28 +117,12 @@ onMounted(async () => {
     <section class="list-item-container" aria-labelledby="link-section-title">
       <h1 id="link-section-title" class="list-title mcfont">友情链接</h1>
       <div class="link-list-scroll">
-        <a
+        <LinkItem
           v-for="(link, i) in linkEntries"
           :key="link.name"
-          class="link-card"
-          :href="link.url"
-          target="_blank"
-          rel="noopener noreferrer"
+          :link="link"
           :style="{ animationDelay: `${i * 0.15}s` }"
-        >
-          <div class="link-item-border">
-            <div class="link-icon-area">
-              <img :src="link.icon" :alt="`${link.name} 图标`" />
-            </div>
-            <div class="link-item-info">
-              <span class="link-item-name">{{ link.name }}</span>
-              <span class="link-item-desc">{{ link.description }}</span>
-            </div>
-            <div class="link-item-meta">
-              <span class="link-item-arrow" aria-hidden="true">▶</span>
-            </div>
-          </div>
-        </a>
+        />
       </div>
     </section>
 
@@ -206,155 +190,5 @@ onMounted(async () => {
   overflow-x: auto;
   display: flex;
   flex-direction: column;
-}
-
-.link-card {
-  width: 100%;
-  max-width: 768px;
-  min-width: 20rem;
-  margin: 0 auto;
-  color: inherit;
-  text-decoration: none;
-
-  opacity: 0;
-  animation: fade-in-right 0.5s ease-in-out forwards;
-}
-
-.link-item-border {
-  position: relative;
-  width: 100%;
-  padding: 2px 4px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  min-width: 20rem;
-  background-size: 100% auto;
-  border: 2px solid transparent;
-  box-sizing: border-box;
-}
-
-.link-card:hover .link-item-border,
-.link-card:focus-visible .link-item-border {
-  background-color: black;
-  border: 2px solid white;
-}
-
-.link-card:focus-visible {
-  outline: none;
-}
-
-.link-icon-area {
-  position: relative;
-  width: 64px;
-  height: 64px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-shrink: 0;
-}
-
-.link-icon-area img {
-  width: 60px;
-  height: 60px;
-  border: 1px solid grey;
-  image-rendering: pixelated;
-  user-select: none;
-}
-
-.link-card:hover .link-icon-area::after {
-  pointer-events: none;
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 64px;
-  height: 64px;
-  background: url('/UI/Arrow_Right.png') no-repeat center;
-  background-size: 22px 34px;
-  image-rendering: pixelated;
-  z-index: 256;
-}
-
-.link-card:hover .link-icon-area::before {
-  pointer-events: none;
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 64px;
-  height: 64px;
-  background: rgba(128, 128, 128, 0.7);
-  z-index: 128;
-}
-
-.link-card:focus-visible .link-icon-area::after {
-  pointer-events: none;
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 64px;
-  height: 64px;
-  background: url('/UI/Arrow_Right.png') no-repeat center;
-  background-size: 22px 34px;
-  image-rendering: pixelated;
-  z-index: 256;
-}
-
-.link-card:focus-visible .link-icon-area::before {
-  pointer-events: none;
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 64px;
-  height: 64px;
-  background: rgba(128, 128, 128, 0.7);
-  z-index: 128;
-}
-
-.link-item-info {
-  display: flex;
-  flex-direction: column;
-  margin-left: 1rem;
-  min-width: 0;
-}
-
-.link-item-name {
-  color: white;
-  line-height: 1.1rem;
-  font-size: 1.1rem;
-  margin-bottom: 5px;
-  margin-top: 4px;
-  user-select: none;
-}
-
-.link-item-desc {
-  line-height: 1rem;
-  user-select: none;
-  color: rgba(255, 255, 255, 0.72);
-}
-
-.link-item-meta {
-  display: flex;
-  align-items: center;
-  margin-left: auto;
-  padding-right: 0.5rem;
-}
-
-.link-item-arrow {
-  color: #aaaaaa;
-  font-size: 0.9rem;
-  user-select: none;
-}
-
-@media screen and (max-width: 768px) {
-  .link-card {
-    min-width: 0;
-  }
-
-  .link-item-border {
-    min-width: 0;
-  }
 }
 </style>
